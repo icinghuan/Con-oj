098904cba1c408a26b9d70d84b1ba9f5eddc89b1
icing幻 <icinghuan@outlook.com>
2016-09-08 20:49:04 +0800

    template

---
+++ RE0/template.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<cstdio>
+#include<string>
+#include<cstring>
+#include<algorithm>
+#include<stack>
+#include<queue>
+#include<map>
+#include<set>
+#include<cmath>
+#include<ctime>
+#define FOR(i,a,b) for(int i=a;i<=b;++i)
+#define FORR(i,a,b) for(int i=a;i>=b;--i)
+#define mp(x,y) make_pair(x,y)
+#define sqr(x) ((x)*(x))
+
+using namespace std;
+
+typedef long long LL;
+
+
+
+int main()
+{
+    int T;
+    scanf("%d",&T);
+    while(T--)
+    {
+        scanf("%d",&);
+
+    }
+    return 0;
+}
